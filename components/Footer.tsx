d5cd467603671d5ccbf8024d3f4b71c2ef61ca98
kdten <kolten.rasmussen@gmail.com>
2023-12-19 23:13:56 -0700

    comment out unnecessary links, faq link updated

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -21,9 +21,9 @@ const footerItems: FooterItems = [
     title: 'Product',
     items: [
       { title: 'Features', href: '/features' },
-      { title: 'Something', href: '/something' },
-      { title: 'Something else', href: '/something-else' },
-      { title: 'And something else', href: '/and-something-else' },
+      { title: 'App store', href: '/appstore' },
+      // { title: 'Something else', href: '/something-else' },
+      // { title: 'And something else', href: '/and-something-else' },
     ],
   },
   {
@@ -31,19 +31,19 @@ const footerItems: FooterItems = [
     items: [
       { title: 'Blog', href: '/blog' },
       { title: 'Contact', href: '/contact' },
-      { title: 'FAQ', href: '/faq' },
-      { title: 'Help Center', href: '/help-center' },
+      { title: 'FAQ', href: '/pricing#FAQ' },
+      // { title: 'Help Center', href: '/help-center' },
     ],
   },
-  {
-    title: 'Something',
-    items: [
-      { title: 'Features2', href: '/features2' },
-      { title: 'Something2', href: '/something2' },
-      { title: 'Something else2', href: '/something-else2' },
-      { title: 'And something else2', href: '/and-something-else2' },
-    ],
-  },
+  // {
+  //   title: 'Something',
+  //   items: [
+  //     { title: 'Features2', href: '/features2' },
+  //     { title: 'Something2', href: '/something2' },
+  //     { title: 'Something else2', href: '/something-else2' },
+  //     { title: 'And something else2', href: '/and-something-else2' },
+  //   ],
+  // },
 ];
 
 export default function Footer() {
