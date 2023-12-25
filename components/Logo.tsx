a6d29faf4243896e3d3ff431010b24669312a9c4
kdten <kolten.rasmussen@gmail.com>
2023-12-25 13:05:18 -0700

    dark and light mode images for navbar correctly used, system prefs still default

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -1,14 +1,42 @@
- import Image from 'next/image'
+import Image from 'next/image'
+import React, { useEffect, useState } from 'react';
 
-  export default function Logo({ ...rest }) {
-  
-    return (
-      <Image
-        src='/v3.png'
-        width={260}
-        height={120}
-        alt="Picture of app on phone"
-      />
-  
-    )
-  }
+export default function Logo({ ...rest }) {
+  // Function to determine the initial logo based on the system settings
+  const getInitialLogo = () => {
+    if (typeof window !== 'undefined') {
+      const isDark = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
+      return isDark ? '/navbarLogoDark.webp' : '/navbarLogoLight.webp';
+    }
+    return '/navbarLogoLight.webp'; // default to light mode logo if window is not defined
+  };
+
+  // State for image source
+  const [logoSrc, setLogoSrc] = useState(getInitialLogo);
+
+  useEffect(() => {
+    // Function to update logo based on theme
+    const updateLogo = () => {
+      const isDark = document.body.classList.contains('next-dark-theme');
+      setLogoSrc(isDark ? '/navbarLogoDark.webp' : '/navbarLogoLight.webp');
+    };
+
+    // Initial check and set up an observer for class changes on body
+    updateLogo();
+    const observer = new MutationObserver(updateLogo);
+    observer.observe(document.body, { attributeFilter: ['class'] });
+
+    // Clean up observer on component unmount
+    return () => observer.disconnect();
+  }, []);
+
+  return (
+    <Image
+      src={logoSrc}
+      width={208}
+      height={79}
+      alt="Coupon Catch logo, featuring a fish and hook"
+      {...rest}
+    />
+  )
+}
